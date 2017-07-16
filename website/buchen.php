ebbd18717b1a57efa78feb12f892fa17a47a97f8
Martin Geisse <martingeisse@googlemail.com>
2017-07-16 12:30:33 +0200

    form validation

--- website/buchen.php
+++ website/buchen.php
@@ -31,33 +31,42 @@ foreach ($_REQUEST as $key => $value) {
 $validationErrors = array();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	global $fields, $validationErrors;
 
 	// TODO
 
 	if (empty($fields['name'])) {
 		$validationErrors['name'] = 'Bitte geben Sie hier Ihren Namen ein.';
 	}
+
+	if (empty($validationErrors)) {
+		// TODO buchen
+		header('Location: uebersicht-besucher.php', true, 302);
+	}
 }
 
 function printValidationError($key) {
 	global $validationErrors;
 	if (!empty($validationErrors[$key])) {
-		echo '<div style="color: red">', $validationErrors[$key], '</div>', "\n";
+		echo '<div class="feedback-message alert alert-danger">', $validationErrors[$key], '</div>', "\n";
 	}
 }
 
 ?>
 <h1>Termin Buchen: <?= $tag ?>.<?= $monat ?>.<?= $jahr ?> <?= zt_zeitpunktText($slotStartzeit) ?> - <?= zt_zeitpunktText($slotEndezeit) ?></h1>
 
-<form method="POST" action="buchen.php">
+<form class="form" method="POST" action="buchen.php">
+<?php /*
 	<?php printValidationError('vonSlot'); ?>
 	<div>von slot # <input type="text" name="von" value="<?= htmlspecialchars($fields['vonSlot']) ?>"></div>
 	<?php printValidationError('bisSlot'); ?>
 	<div>bis slot # <input type="text" name="bis" value="<?= htmlspecialchars($fields['bisSlot']) ?>"></div>
+*/ ?>
+
+	<div><label for="name">Name</label></div>
 	<?php printValidationError('name'); ?>
-	<div>Name <input type="text" name="name" value="<?= htmlspecialchars($fields['name']) ?>"></div>
-	<div><input type="submit" value="buchen"> oder <a href="uebersicht-besucher.php">zurück</a></div>
+	<div><input class="form-control" type="text" name="name" value="<?= htmlspecialchars($fields['name']) ?>"></div>
+	<br>
+	<div><input class="btn btn-primary" type="submit" value="buchen"> oder <a href="uebersicht-besucher.php">zurück</a></div>
 </form>
 
 <?php require('_outro.php');
