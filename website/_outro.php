d1934e22a078e2ba909e97221bcb5dde8826e820
Martin Geisse <martingeisse@googlemail.com>
2017-07-20 21:51:59 +0200

    postMessage to resize the iframe to the actual content height

--- website/_outro.php
+++ website/_outro.php
@@ -8,5 +8,8 @@ if (!empty($databaseConnection)) {
 }
 ?>
 </div>
+<script type="text/javascript">
+	window.parent.postMessage('iframe-embedding:' + document.body.scrollHeight,'*');
+</script>
 </body>
 </html>
