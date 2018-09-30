845ac2cd0bcee478776e3821f13afa6ab06ce995
Martin Geisse <martingeisse@googlemail.com>
2018-09-30 17:04:48 +0200

    Einleitungstext angepasst

--- website/start.php
+++ website/start.php
@@ -16,11 +16,10 @@ Bitte beachten Sie:
 <ul>
 <li>Geben Sie die Menge bei der Buchung möglichst genau an. Größere Mengen können an Ihrem Wunschtermin nicht
 	verarbeitet werden.</li>
-<li>Wollen Sie Birnen, Quitten oder Trauben keltern, ist es ratsam, den ersten Termin am Tag zu buchen, weil ein
-	Reinigen der Anlage im laufenden Betrieb nicht möglich ist.</li>
 <li>Eine Terminbuchung ist verbindlich und kann nur telefonisch oder per Email spätestens einen Tag vor
 	dem Termin storniert werden. Ansonsten fallen 50% der Keltergebühren an.</li>
 <li>Wir bitten, Termine für Mengen über 20 Zentner = 1000Kg weiterhin telefonisch zu vereinbaren.</li>
+<li>Das Erhitzen und Abfüllen von Saft an der Anlage in Volpertshausen ist nicht möglich, bitte beachten Sie den Hinweis unter AKTUELLES.</li>
 </ul>
 <p>
 <a href="uebersicht.php" class="btn btn-primary">Zum Buchungssystem</a>
